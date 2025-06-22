a05eae5164dc612a9e96dfddc064a0d7f2d4cb0c
Benni Schwerdtner <Benjamin.Schwerdtner@gmail.com>
2025-06-22 21:13:58 +0200

    Curr

--- publish.sh
+++ publish.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# rsync -avz run.sh  ionos-1:/var/www/ftlm-vehicles
-# rsync -avz target ionos-1:/var/www/ftlm-vehicles
-# rsync -avz release.jar ionos-1:/var/www/ftlm-vehicles
+rsync -avz run.sh  linode:/var/www/ftlm-vehicles
+rsync -avz target linode:/var/www/ftlm-vehicles
+rsync -avz release.jar linode:/var/www/ftlm-vehicles
 
-rsync -avz run.sh  ionos-1:/vehicles
-rsync -avz target ionos-1:/vehicles
-rsync -avz release.jar ionos-1:/vehicles
+# rsync -avz run.sh  ionos-1:/vehicles
+# rsync -avz target ionos-1:/vehicles
+# rsync -avz release.jar ionos-1:/vehicles
